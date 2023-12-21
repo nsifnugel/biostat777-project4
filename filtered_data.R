7a552837fd487794d883ce6c58ab8c5ded45848d
nsifnugel <nsifnug1@jh.edu>
2023-12-21 13:59:18 -0500

    final edits

--- filtered_data.R
+++ filtered_data.R
@@ -2,19 +2,15 @@ library(tidyverse)
 library(here)
 library(tidycensus)
 library(rvest)
+options(tigris_use_cache = TRUE)
 
 total_using_public_transportation <- c("B08301_010E")
-
 total_commuting <- c("B08301_001E")
-
 by_earnings <- c("B08119_029E","B08119_030E","B08119_031E","B08119_032E","B08119_033E","B08119_034E","B08119_035E","B08119_036E")
-
 by_vehicles <- c("B08141_017E","B08141_018E","B08141_019E","B08141_020E")
-
 variables <- c(total_using_public_transportation, total_commuting,by_earnings,by_vehicles)
 
 boston <- c(02101:02137, 02163,02199,02203,02205,02208,02209,02215,02222,02228,02283,02284,02455)
-
 
 new_york <- read_html("https://www.nycbynatives.com/nyc_info/new_york_city_zip_codes.php") %>%  
   html_elements(".grippy-host , td:nth-child(4) , td:nth-child(1)") %>% 
@@ -22,16 +18,11 @@ new_york <- read_html("https://www.nycbynatives.com/nyc_info/new_york_city_zip_c
   c() %>% 
   as.numeric()
   
-
-  
-
 philadelphia <- read_html("https://www.ciclt.net/sn/clt/capitolimpact/gw_ziplist.aspx?FIPS=42101") %>%  
   html_elements("td td td:nth-child(1) a") %>% 
   html_text2() %>% 
   c() %>% 
   as.numeric()
-
-
 
 dc <- read_html("https://www.ciclt.net/sn/clt/capitolimpact/gw_ziplist.aspx?FIPS=11001&stfips=&state=DC&stname=") %>%  
   html_elements("td td td:nth-child(1) a") %>% 
